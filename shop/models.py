20ca6c7929251e724be92c00ba3fa5313256f949
Vansh <vanshpthakkar@gmail.com>
2023-07-07 11:42:50 +0530

    fourth commit

--- shop/models.py
+++ shop/models.py
@@ -16,5 +16,8 @@ class Product(models.Model):
 #first we run migrate command, then add table to our models.py file. as soon as we add tables in our models.py, we have to change from appname in installed apps to appname.apps.AppnameConfig(shop.apps.ShopConfig) ,then we have to run python manage.py makemigrations. after that we finally do the migrations by python manage.py migrate. We can see our database made here in models.py by django admin. for acccessing django admin page, we need to create a superuser which could only access django/admin page. for that write pytohn manage.py createsuperuser
 #we have to register now our model on admin.py
 
+#IMPORTANT
 #we can upload images here inside images but this is not recommended manner. we use media directory for storing all media items.
-#
+#see how to add media files in settings.py file in every website. after doing that stuff, do changes in urls.py of mac, see what changes we have done and do it on every website
+
+#to show product information from database into front end, do following steps
--- shop/views.py
+++ shop/views.py
@@ -1,22 +1,46 @@
 from django.shortcuts import render
+from .models import Product
+from math import ceil
+
+# Create your views here.
 from django.http import HttpResponse
 
 def index(request):
-    return render(request,'shop/index.html')
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nSlides = n//4 + ceil((n/4)-(n//4))
+
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    print(catprods)
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+    # params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    params = {'allProds':allProds}
+    return render(request, 'shop/index.html', params)
+
 def about(request):
-    return HttpResponse("page")
+    return render(request, 'shop/about.html')
+
 def contact(request):
-    return HttpResponse("page")
+    return HttpResponse("We are at contact")
+
 def tracker(request):
-    return HttpResponse("Page")
+    return HttpResponse("We are at tracker")
+
 def search(request):
-    return HttpResponse("page")
+    return HttpResponse("We are at search")
+
 def prodview(request):
-    return HttpResponse("page")
+    return HttpResponse("We are at product view")
+
 def checkout(request):
-    return HttpResponse("page")
-
-
-
-
-# Create your views here.
+    return HttpResponse("We are at checkout")
